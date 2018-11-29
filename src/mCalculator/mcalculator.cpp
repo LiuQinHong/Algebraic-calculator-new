c636b2fbc08f43bc0ce4526cc41a3436c88a6670
liujl0702 <liujl0702@thundersoft.com>
2018-11-29 17:20:57 +0800

    liujl : 修改displayText

--- src/mCalculator/mcalculator.cpp
+++ src/mCalculator/mcalculator.cpp
@@ -29,7 +29,7 @@ mCalculator::mCalculator(QWidget *parent) :
 
     ui->label_name->setAlignment(Qt::AlignHCenter);
 
-    ui->textEdit_display->setText("<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">(a+b)<span style=\" vertical-align:super;\">(a+b)</span></p>");
+    //ui->textEdit_display->setText("<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">(a+b)<span style=\" vertical-align:super;\">(a+b)</span></p>");
     ItemList den;
     ItemList mole;
     ItemList::process("(((a+b)^2/b)^c + g*h/a)/(d/e)^f", &den, &mole);
@@ -56,10 +56,6 @@ mCalculator::mCalculator(QWidget *parent) :
     Merge merge2(test);
     merge2.makeItem(test);*/
 
-
-
-    displayText(den,mole);
-
 }
 
 mCalculator::~mCalculator()
@@ -75,16 +71,20 @@ void mCalculator::paintEvent(QPaintEvent *event){
 void mCalculator::displayText(ItemList &den, ItemList &mole)
 {
     int length = den.mExpressionStr.length() - mole.mExpressionStr.length();
-    qDebug() << "length = " << length;
-    Transform tt(mole,true,length);
-    tt.transform();
-    Transform tt1(den,false,length);
-    tt1.transform();
-    QString QoutHtml(tt.getOutHtml()->c_str());
-    QString QoutHtml1(tt1.getOutHtml()->c_str());
-    QoutHtml += QoutHtml1;
 
-    ui->textEdit_display->setText(QoutHtml);
+    Transform transfromMole(mole,true,length);
+    transfromMole.transform();
+    Transform transfromDen(den,false,length);
+    transfromDen.transform();
+
+    QString outHtmlMole(transfromMole.getOutHtml()->c_str());
+    QString outHtmlDen(transfromDen.getOutHtml()->c_str());
+
+    outHtmlMole += outHtmlDen;
+
+    ui->textEdit_display->append(outHtmlMole);
+    ui->textEdit_display->append("<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; "
+                                 "-qt-block-indent:0; text-indent:0px;\"></p>");
 }
 
 void mCalculator::on_pushButton_equal_clicked()
